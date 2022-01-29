6b0213fe8edcda5d5290d0d7ea8e67bb1dbfe311
Igor Nalyvaiko <igor4mail@gmail.com>
2022-01-29 20:51:20 +0200

    Initial commit

---
+++ version-repository/src/main/java/nalyvaiko/versionrepository/VersionRepositoryApplication.java
@@ -0,0 +1,13 @@
+package nalyvaiko.versionrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionRepositoryApplication.class, args);
+	}
+
+}
---
+++ version-repository/src/test/java/nalyvaiko/versionrepository/VersionRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package nalyvaiko.versionrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
